0c3f1e5e06a19b7c51c637b2c0778fb1ac61e8a6
CaptainBlagbird <CaptainBlagbird@mailueberfall.de>
2015-02-08 18:26:27 +0100

    Added slash option to auto show helmet when in combat

--- HelmetToggle/HelmetToggle.lua
+++ HelmetToggle/HelmetToggle.lua
@@ -11,6 +11,33 @@ HelmetToggle = {}
 -- Name string
 HelmetToggle.name = "HelmetToggle"
 
+-- Convert string to boolean by using a list of keywords
+local function tobool(str)
+	local list = {}
+	list["true"]  = true
+	list["on"]    = true
+	list["1"]     = true
+	list["false"] = false
+	list["off"]   = false
+	list["0"]     = false
+	
+	-- If input is "?", then return a list of all possible options
+	if str == "?" then
+		local strKeys = ""
+		for k,v in pairs(list) do
+			if strKeys == "" then
+				strKeys = k
+			else
+				strKeys = strKeys .. " / " .. k
+			end
+		end
+		return strKeys
+	end
+	
+	-- Return the value for that key
+	return list[str]
+end
+
 -- Event handler function, called when the UI was moved
 function HelmetToggle.OnUIMoveStop()
 	HelmetToggle.savedVariables.left = HelmetToggleUI:GetLeft()
@@ -19,14 +46,7 @@ end
 
 -- Event handler function, called when the button was clicked
 function HelmetToggle.OnButtonClicked()
-	-- Get setting of the hide/show helmet option
-	local before = GetSetting( SETTING_TYPE_IN_WORLD, IN_WORLD_UI_SETTING_HIDE_HELM )
-	-- Toggle the hide/show helmet option
-	if before == "1" then
-		SetSetting( SETTING_TYPE_IN_WORLD, IN_WORLD_UI_SETTING_HIDE_HELM, "0", 1 )
-	else
-		SetSetting( SETTING_TYPE_IN_WORLD, IN_WORLD_UI_SETTING_HIDE_HELM, "1", 1 )
-	end
+	HelmetToggle.ToggleHelmet()
 end
 
 -- Function to restore the position of the Button
@@ -62,11 +82,75 @@ function HelmetToggle.OnUIUpdate()
 		HelmetToggleUI:SetHidden(true)
 	end
 end
+
+-- Event handler function, called when the combat state changed
+function HelmetToggle.OnCombatStateChange(event, inCombat)
+	HelmetToggle.ToggleHelmet(inCombat)
+end
+
+-- Function for slash command
+function HelmetToggle.ChatCommand(argument)
+	-- Trim whitespaces from input string
+	argument = argument:gsub("^%s*(.-)%s*$", "%1")
+	-- Convert string to lower case
+	argument = argument:lower()
+	-- No argument toggles current mode
+	if argument == "" then
+		if HelmetToggle.savedVariables.auto == nil then
+			HelmetToggle.savedVariables.auto = true
+		else
+			HelmetToggle.savedVariables.auto = not HelmetToggle.savedVariables.auto
+		end
+	else
+		local boolArg = tobool(argument)
+		if boolArg == nil or argument == "?" then
+			-- Display error message
+			d("|c70C0DE[HelmetToggle]|r Invalid argument, valid options: |cC5C29E" .. tobool("?") .. " and no argument|r")
+			return
+		else
+			HelmetToggle.savedVariables.auto = boolArg
+		end
+	end
+	-- Start of output string
+	local str = "|c70C0DE[HelmetToggle]|r Auto mode "
+	if HelmetToggle.savedVariables.auto then
+		-- Register function for event
+		EVENT_MANAGER:RegisterForEvent(HelmetToggle.name, EVENT_PLAYER_COMBAT_STATE, HelmetToggle.OnCombatStateChange)
+		str = str .. "|c00FF00enabled|r"
+	else
+		-- Unregister function for event
+		EVENT_MANAGER:UnregisterForEvent(HelmetToggle.name, EVENT_PLAYER_COMBAT_STATE)
+		str = str .. "|cFF0000disabled|r"
+	end
+	-- Finally display output string with changed mode
+	d(str)
+end
+
+-- Event handler function, called when the button was clicked
+function HelmetToggle.ToggleHelmet(arg)
+	-- Check if argument specified
+	if arg ~= nil then
+		-- Use argument as new setting
+		SetSetting(SETTING_TYPE_IN_WORLD, IN_WORLD_UI_SETTING_HIDE_HELM, tostring(not arg), 1)
+	else
+		-- No argument specified, toggle current setting
+		local old = GetSetting(SETTING_TYPE_IN_WORLD, IN_WORLD_UI_SETTING_HIDE_HELM)
+		SetSetting(SETTING_TYPE_IN_WORLD, IN_WORLD_UI_SETTING_HIDE_HELM, tostring(not tobool(old)), 1)
+	end
+end
  
 -- Initialisations
 function HelmetToggle:Init()
 	-- Set up SavedVariables object
 	self.savedVariables = ZO_SavedVars:New("HelmetToggleSavedVariables", 1, nil, {})
+	
+	-- Register slash command and link function
+	SLASH_COMMANDS["/helmettoggle"] = HelmetToggle.ChatCommand
+	
+	-- Make sure the event is registered if auto mode is on
+	if HelmetToggle.savedVariables.auto then
+		EVENT_MANAGER:RegisterForEvent(HelmetToggle.name, EVENT_PLAYER_COMBAT_STATE, HelmetToggle.OnCombatStateChange)
+	end
 	
 	-- Restore button position
 	self:RestoreButtonPosition()
